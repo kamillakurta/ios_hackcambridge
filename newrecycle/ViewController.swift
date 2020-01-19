29f37b240da44151401a44fe1d709840de0f25fb
kamillakurta <Kke282!!>
2020-01-19 10:14:23 +0000

    Initial Commit

---
+++ newrecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newrecycle
+//
+//  Created by Kamilla Kurta on 2020. 01. 19..
+//  Copyright © 2020. hackathon - cambridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
